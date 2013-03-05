9d28dec5981f9fb85ca1134183fd4a98b783bf34
BIGEON Emmanuel <emmanuel.bigeon@epfl.ch>
2013-03-05 13:41:07 +0100

    Adding a debug verison of the dsl.print macro

--- components/dsls/vector/src/dsl/print/package.scala
+++ components/dsls/vector/src/dsl/print/package.scala
@@ -8,8 +8,11 @@ import scala.tools.reflect.ToolBoxFactory
 object `package` {
 
   def liftPrint[T](block: ⇒ T): T = macro _liftPrint[T]
+  def liftPrintDebug[T](block: ⇒ T): T = macro _liftPrintDebug[T]
   def _liftPrint[T](c: Context)(block: c.Expr[T]): c.Expr[T] =
     new MPDETransformer[c.type, T](c, "dsl.print.PrintDSL")(block)
+  def _liftPrintDebug[T](c: Context)(block: c.Expr[T]): c.Expr[T] =
+    new MPDETransformer[c.type, T](c, "dsl.print.PrintDSL", true)(block)
 
   // The only thing we declare here
   def println(x: Any) = ???
